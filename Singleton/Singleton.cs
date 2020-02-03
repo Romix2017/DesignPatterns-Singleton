c204fa95ae628099cb7245b7a3925f2801c01346
Roman Karpov <poma.05@mail.ru>
2020-02-03 16:39:14 +0400

    init

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        protected Singleton() { }
+        public static Singleton Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
---
+++ SingletonRealWorld/LoadBalancer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonRealWorld
+{
+    public class LoadBalancer
+    {
+        private static LoadBalancer instance;
+        private List<string> servers = new List<string>();
+        private Random random = new Random();
+        private static object syncLock = new object();
+        protected LoadBalancer()
+        {
+            servers.Add("Server1");
+            servers.Add("Server2");
+            servers.Add("Server3");
+            servers.Add("Server4");
+            servers.Add("server5");
+        }
+        public static LoadBalancer GetLoadBalancer()
+        {
+            if (instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LoadBalancer();
+                    }
+                }
+            }
+            return instance;
+        }
+        public string Server
+        {
+            get
+            {
+                int r = random.Next(servers.Count);
+                return servers[r].ToString();
+            }
+        }
+    }
+}
